a2f11a124557b3f9cfa8ee8629bda78d80b8a0a9
Jimmy Bergvall <den_dar_jimmy@hotmail.com>
2017-10-17 11:56:08 +0200

    Add highScore

--- src/com/company/GameOver.java
+++ src/com/company/GameOver.java
@@ -1,4 +1,67 @@
 package com.company;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class GameOver {
-}
+
+    private List<Integer> highScore = new ArrayList<>();
+
+    public void endGame(int points, Terminal terminal)throws IOException {
+
+        String input = "highscore.txt";
+        File f = new File((input));
+        if (f.exists()) {
+            Scanner sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                highScore.add(sc.nextInt());
+            }
+            highScore.add(points);
+            Collections.sort(highScore);
+        }
+        else{
+            f.createNewFile();
+            highScore.add(points);
+        }
+
+
+        // creates a FileWriter Object
+
+        FileWriter writer = new FileWriter(input);
+
+
+
+
+
+        for (Integer point: highScore) {
+            writer.write(point);
+            System.out.println(point);
+        }
+        // Writes the content to the file
+        writer.flush();
+        writer.close();
+
+        terminal.clearScreen();
+        int y = 10;
+
+
+        for (Integer p:highScore) {
+            String temp = p.toString();
+
+            terminal.moveCursor(10,y++);
+            for (int i = 0; i < temp.length(); i++) {
+                terminal.putCharacter(temp.charAt(i));
+            }
+
+            }
+        }
+
+
+    }
+
+
---
+++ src/com/company/Test.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+
+        Terminal terminal = TerminalFacade.createTerminal(System.in,
+                System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+
+
+        GameOver gameOver = new GameOver();
+
+
+        gameOver.endGame(20,terminal);
+
+    }
+}
